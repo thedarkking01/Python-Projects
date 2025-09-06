1522d242c78f55cafa093d9039296992c3be5369
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-09-07 00:15:59 +0530

    complete project

---
+++ Calcy/main.py
@@ -0,0 +1,41 @@
+while True:
+    print()
+    print("Python Calculator")
+    print("=" * 20)
+    print()
+
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    operator = input("Enter an operation (+, -, *, /, **): ")
+    print("You chose:", operator)
+    print()
+    result = 0
+    if operator == '+':
+        result = num1 + num2
+        print(f"{num1} + {num2} = {result}")
+    elif operator == '-':
+        result = num1 - num2
+        print(f"{num1} - {num2} = {result}")
+    elif operator == '*':
+        result = num1 * num2
+        print(f"{num1} * {num2} = {result}")
+    elif operator == '**':
+        result = num1 ** num2
+        print(f"{num1} ** {num2} = {result}")
+    elif operator == '/' and num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    elif operator == '/' and num2 == 0:
+        print("Cannot divide by Zero!")
+    else:
+        print("Invalid Operator!")
+
+    print()
+
+    reset = input("Press 'C' to calculate again. Press any other key to exit! ")
+
+    if reset not in ("C", "c"):
+        print("GOODBYE!")
+        print()
+        break
