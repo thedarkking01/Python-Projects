88ce9f5de1d8ac6431602c5c6162a76e8bcf19bb
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-09-17 22:14:49 +0530

    Add new project

---
+++ Number Guessing Game/main.py
@@ -0,0 +1,5 @@
+print("Welcome to number guessing game.")
+right_number=3
+print("I've picked a number for you to guess.")
+print("The number is between 1 and 5.")
+guessed_number=int(input("Guess a number:"))
