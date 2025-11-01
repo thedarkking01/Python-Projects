a948b108ba555773618c300a0e59f47b0ab2551a
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-11-01 23:39:18 +0530

    add new question

---
+++ interview question/Basic/Merge Two Lists of Different Types.py
@@ -0,0 +1,13 @@
+# replace ___ with your code
+
+# list of string types
+list1 = ['apple', 'orange', 'banana']
+
+# list of numbers
+list2 = [5, 8, 10]
+
+# merge two lists
+merged_list=list1+list2
+
+# print the merged_list
+print(merged_list)
