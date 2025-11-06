0ec018b31bbfad2ab6c1c32c94c186559c20fb90
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-11-06 23:45:39 +0530

    add new question

---
+++ interview question/Basic/Find the Length of the String Without the len() Function.py
@@ -0,0 +1,18 @@
+# replace ___ with your code
+
+# define a function that prints the length of a string without len() function
+def length_of_string(string):
+
+    # define a counter that stores the length of a string
+    length = 0
+
+    # loop through each character in the string
+    for char in string:
+
+        # for each character, increment the length counter by 1
+        length += 1
+    # print the length counter
+    print(length)
+
+# call the function
+length_of_string('Python')
