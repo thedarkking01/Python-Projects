4b96a81bc7612397c2cb7a76bcc4529a1d951af9
thedarkking01 <sourabhsinghbais52@gmail.com>
2025-11-18 22:38:09 +0530

    add new question

---
+++ interview question/Basic/Sorted Words in a String.py
@@ -0,0 +1,18 @@
+# replace ___ with your code
+
+# given string
+sentence = 'python is easy'
+
+# split the string into words
+# by default, python splits by space
+# and returns a list of splitted elements
+words = sentence.split(" ")
+
+# sort the list of words
+# sort() is a built-in function and
+# it sorts the list in ascending order
+words.sort()
+
+# loop through each word and print it
+for i in words:
+    print(i)
